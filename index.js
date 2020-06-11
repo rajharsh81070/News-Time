83fa5a828b782bbcb4a97d3553fe1db68fa2cf3c
rajharsh81070 <rajharsh81070@gmail.com>
2020-06-11 21:01:00 +0530

    Added encryption

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World");
-})
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB!');
+  }
+);
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Middleware
+app.use(express.json());
+
+// Routes Middlewares
+app.use('/user', authRoute);
 
 app.listen(5000, () => {
   console.log("App listening on 5000");
---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+
+router.post('/register', async (req, res) => {
+  // Check if the email is already registered.
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already Exists!');
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create a New User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+  try {
+    const savedUser = await user.save();
+    // console.log(savedUser);
+    res.send({ id: user._id });
+  } catch (err) {
+    // console.log(err);
+    res.status(400).send(err);
+  }
+});
+
+router.post('/login', (req, res) => {
+  // Check if the user is already registered.
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email or Password is wrong");
+
+  // Password is Correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+});
+
+
+module.exports = router;
