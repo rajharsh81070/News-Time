435aff235ab9777d043c8f5c70fd363a8f55a82c
rajharsh81070 <rajharsh81070@gmail.com>
2020-07-24 00:00:52 +0530

    Footer Added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 
-
-const useStyles = makeStyles((theme) => ({
-  footer: {
-    marginTop: theme.spacing.unit * 27,
-    width: theme.spacing.unit
-    // padding: `${theme.spacing.unit * 1}px 0`,
-  }
-}));
-
-function Footer(props) {
-  const classes = useStyles();
-
+function Copyright() {
   return (
-    <AppBar className={classes.footer} position="static" color="primary">
-      <Container maxWidth="100%">
-        <Toolbar>
-          <Typography variant="body1" color="inherit">
-            &copy; 2019 Gistia
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar >
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default Footer;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  footer: {
+    padding: theme.spacing(5, 3),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
+
+export default function Footer() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="lg">
+          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
