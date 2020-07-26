8a091df633867d4d35b3af4807b6f7e6fa776b7c
rajharsh81070 <rajharsh81070@gmail.com>
2020-07-27 03:19:06 +0530

    Login Page Modified

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -89,73 +89,75 @@ function Login(props) {
   }
 
   return (
-    <Container style={{ paddingTop: '90px', paddingBlockEnd: '381px' }} component="main" maxWidth="xs">
+    <>
       <CssBaseline />
       <NavBar />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
+      <Container style={{ paddingTop: '90px', paddingBlockEnd: '381px' }} component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
         </Typography>
-        <form onChange={handleFormChange} onSubmit={handleFormSubmit} className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                error={(!errors.hasOwnProperty('email') || errors.email.length === 0) ? false : true}
-                helperText={errors.email}
-                required={true}
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
+          <form onChange={handleFormChange} onSubmit={handleFormSubmit} className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  error={(!errors.hasOwnProperty('email') || errors.email.length === 0) ? false : true}
+                  helperText={errors.email}
+                  required={true}
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required={true}
+                  error={(!errors.hasOwnProperty('password') || errors.password.length === 0) ? false : true}
+                  helperText={errors.password}
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required={true}
-                error={(!errors.hasOwnProperty('password') || errors.password.length === 0) ? false : true}
-                helperText={errors.password}
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              {/* <Link href="#" variant="body2">
+            <Grid container>
+              <Grid item xs>
+                {/* <Link href="#" variant="body2">
                 Forgot password?
               </Link> */}
+              </Grid>
+              <Grid item>
+                <Link to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link to="/register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      {/* <Box mt={8}>
+          </form>
+        </div>
+        {/* <Box mt={8}>
         <Copyright />
       </Box> */}
-    </Container >
+      </Container >
+    </>
   );
 }
 
